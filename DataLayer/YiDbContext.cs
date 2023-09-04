e031d96d16234f49649a3460bb9e3796ab6162a9
Zsolt Toth <zzt@emri.dk>
2023-09-04 11:03:33 +0200

    YiDbContext Pt.1.

---
+++ DataLayer/YiDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+
+public class YiDbContext : DbContext
+    {
+        public DbSet<Language> Languages { get; set; }
+    }
+
+    public class Language
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class LineText
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Text
+    {
+        public int Id { get; set; }
+        public int HexaGramId { get; set; }
+    }
+
+    public class Hexagram
+    {
+        public int Id { get; set; }
+    }
+
+    public class History
+    {
+        public int Id { get; set; }
+        public int HexaGramId { get; set; }
+        public string Question { get; set;}
+    }
+
+
+}
