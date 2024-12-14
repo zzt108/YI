9749e63aa59e4876cee7939ce819376b570e39ca
zzt108 <gerzson63@gmail.com>
2024-12-14 18:18:39 +0100

    Added Test2_12 which later fails

--- TestHexagram/TestHexagram.cs
+++ TestHexagram/TestHexagram.cs
@@ -28,6 +28,26 @@ public class TestHexagram
         hexagram.Changed.Should().Be(12);
         hexagram.ChangingLines.Should().BeEquivalentTo(new[] {1 , 2, 3, 4, 5, 6});
     }
+
+    [Test]
+    public void Test2_12()
+    {
+        int[,] data = new int[,]
+        {
+    {0, 0, 0},
+    {0, 0, 0},
+    {0, 0, 0},
+    {1, 0, 0},
+    {1, 0, 0},
+    {1, 0, 0}
+
+    };
+        var hexagram = new HexagramNS.Hexagram(new Values().InitValues(data, (item, row, col) => item > 0));
+        hexagram.Main.Should().Be(2);
+        hexagram.Changed.Should().Be(12);
+        hexagram.ChangingLines.Should().BeEquivalentTo(new[] {  4, 5, 6 });
+    }
+
     [Test]
     public void Test03_12()
     {
