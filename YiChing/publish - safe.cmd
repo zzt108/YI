5a91b336488c9951a34fba8a40e8e6bd3428bcc4
zzt108 <10920613+zzt108@users.noreply.github.com>
2025-01-20 17:19:17 +0100

    added compile to publish - safe.cmd

--- YiChing/publish - safe.cmd
+++ YiChing/publish - safe.cmd
@@ -1,4 +1,5 @@
 rem See g:\My Drive\Sys\Backup\AndroidKeystore\
 dotnet restore
+dotnet build Yi.sln -c Release
 dotnet publish -f net8.0-android34.0 -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=yiching.keystore -p:AndroidSigningKeyAlias=yiching -p:AndroidSigningKeyPass={password} -p:AndroidSigningStorePass={password}
 pause
